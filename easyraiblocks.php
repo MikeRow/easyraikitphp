c918d674855b05dabe17ea47ad4873baa0c6a5c4
mikerow <otta88.box@gmail.com>
2017-02-12 16:34:34 +0100

    Add files via upload

---
+++ easyraiblocks.php
@@ -0,0 +1,210 @@
+<?php
+/*
+
+easyraiblocksphp
+
+===================
+
+INSPIRED BY Andrew LeCody EasyBitcoin-PHP
+
+A simple class for making calls to Bitcoin's API using PHP.
+https://github.com/aceat64/EasyBitcoin-PHP
+
+====================
+
+LICENSE: Use it as you want!
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+
+====================
+
+// Include the script in your configuration file
+include_once('PATH/easyraiblocks.php');
+
+// Initialize RaiBlocks connection/object
+$raiblocks = new RaiBlocks();
+
+// Optionally, you can specify a host and port.
+$raiblocks = new RaiBlocks('host','port');
+// Defaults are:
+//	host = localhost
+//	port = 7076
+//	proto = http
+
+// If you wish to make an SSL connection you can set an optional CA certificate or leave blank
+// This will set the protocol to HTTPS and some CURL flags
+$raiblocks->setSSL('/full/path/to/mycertificate.cert');
+
+// Make calls to node as methods for your object. Responses are returned as an array.
+// Example:
+
+$args = array(
+	"account" => "xrb_3e3j5tkog48pnny9dmfzj1r16pg8t1e76dz5tmac6iq689wyjfpi00000000"
+);
+
+$response = $raiblocks->account_balance( $args );
+echo $response['balance'];
+
+// The full response (not usually needed) is stored in $this->response while the raw JSON is stored in $this->raw_response
+
+// When a call fails for any reason, it will return FALSE and put the error message in $this->error
+// Example:
+echo $raiblocks->error;
+
+// The HTTP status code can be found in $this->status and will either be a valid HTTP status code or will be 0 if cURL was unable to connect.
+// Example:
+echo $raiblocks->status;
+
+*/
+
+DEFINE("RAI", "000000000000000000000000"); // A string to concatenate to build 1Rai value
+DEFINE("RAIN", "1000000000000000000000000"); // A value to build 1Rai value
+
+class RaiBlocks {
+    // Configuration options
+    private $proto;
+    private $host;
+    private $port;
+    private $url;
+    private $CACertificate;
+
+    // Information and debugging
+    public $status;
+    public $error;
+    public $raw_response;
+    public $response;
+
+    private $id = 0;
+
+    /**
+     * @param string $host
+     * @param int $port
+     * @param string $proto
+     * @param string $url
+     */
+    function __construct( $host = 'localhost', $port = 7076, $url = null ) {
+        $this->host          = $host;
+        $this->port          = $port;
+        $this->url           = $url;
+        // Set some defaults
+        $this->proto         = 'http';
+        $this->CACertificate = null;
+    }
+
+    /**
+     * @param string|null $certificate
+     */
+    function setSSL($certificate = null) {
+        $this->proto         = 'https'; // force HTTPS
+        $this->CACertificate = $certificate;
+    }
+
+    function __call($method, $params) {
+        $this->status       = null;
+        $this->error        = null;
+        $this->raw_response = null;
+        $this->response     = null;
+
+        // If no parameters are passed, this will be an empty array
+        //$params = array_values($params);
+
+        // The ID should be unique for each call
+        $this->id++;
+
+        // Build the request, it's ok that params might have any empty array
+        $request = array(
+            'action' => $method
+            //'params' => $params
+            //'id'     => $this->id
+        );
+		
+        if( isset($params[0]) ){
+        
+		foreach($params[0] as $key=>$value){
+				
+			$request[$key] = $value;
+				
+		}
+		
+        }
+		
+	$request = json_encode($request);
+
+        // Build the cURL session
+        $curl    = curl_init("{$this->proto}://{$this->host}:{$this->port}/{$this->url}");
+        $options = array(
+            CURLOPT_RETURNTRANSFER => TRUE,
+            CURLOPT_FOLLOWLOCATION => TRUE,
+            CURLOPT_MAXREDIRS      => 10,
+            CURLOPT_HTTPHEADER     => array('Content-type: application/json'),
+            CURLOPT_POST           => TRUE,
+            CURLOPT_POSTFIELDS     => $request
+        );
+
+        // This prevents users from getting the following warning when open_basedir is set:
+        // Warning: curl_setopt() [function.curl-setopt]: CURLOPT_FOLLOWLOCATION cannot be activated when in safe_mode or an open_basedir is set
+        if (ini_get('open_basedir')) {
+            unset($options[CURLOPT_FOLLOWLOCATION]);
+        }
+
+        if ($this->proto == 'https') {
+            // If the CA Certificate was specified we change CURL to look for it
+            if ($this->CACertificate != null) {
+                $options[CURLOPT_CAINFO] = $this->CACertificate;
+                $options[CURLOPT_CAPATH] = DIRNAME($this->CACertificate);
+            }
+            else {
+                // If not we need to assume the SSL cannot be verified so we set this flag to FALSE to allow the connection
+                $options[CURLOPT_SSL_VERIFYPEER] = FALSE;
+            }
+        }
+
+        curl_setopt_array($curl, $options);
+
+        // Execute the request and decode to an array
+        $this->raw_response = curl_exec($curl);
+        $this->response     = json_decode($this->raw_response, TRUE);
+
+        // If the status is not 200, something is wrong
+        $this->status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+        
+        // If there was no error, this will be an empty string
+        $curl_error = curl_error($curl);
+
+        curl_close($curl);
+
+        if (!empty($curl_error)) {
+            $this->error = $curl_error;
+        }
+
+        if ($this->status != 200) {
+            // If node didn't return a nice error message, we need to make our own
+            switch ($this->status) {
+                case 400:
+                    $this->error = 'HTTP_BAD_REQUEST';
+                    break;
+                case 401:
+                    $this->error = 'HTTP_UNAUTHORIZED';
+                    break;
+                case 403:
+                    $this->error = 'HTTP_FORBIDDEN';
+                    break;
+                case 404:
+                    $this->error = 'HTTP_NOT_FOUND';
+                    break;
+            }
+        }
+
+        if ($this->error) {
+            return FALSE;
+        }
+
+        return $this->response;
+    }
+}
---
+++ easyraiext.php
@@ -0,0 +1,233 @@
+<?php
+
+	/*
+
+	easyraiextphp
+
+	Allows you to perform some advanced operation not available with RPC
+	
+	====================
+
+	LICENSE: Use it as you want!
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+	THE SOFTWARE.
+
+	====================
+
+	*/
+
+	// *******************
+	// USAGE OF THE SCRIPT
+	// *******************
+
+	/*
+	
+	To use this extension add after include_once('PATH/easyraiblocks.php');
+	This: include_once('PATH/easyraiext.php');
+	
+	*/
+	
+	$rb_ext = // Put here your variable name used to call RPC, Example: $raiblocks = new RaiBlocks('host','port');
+	
+	// ************************************************************
+	// DO NOT EDIT BELOW, BUT DO IT IF YOU KNOW WHAT YOU ARE DOING!
+	// ************************************************************
+	
+	// Call this function to get all balances of every accounts in a wallet
+	// Parameters:
+	// $walletID -> the ID of the wallet you want to check
+	
+	function raiblocks_balance_wallet($walletID){
+	
+		global $rb_ext;
+		$accounts_balances = array( "sum_balance" => 0, "sum_pending" => 0, "accounts" => array() );
+		
+		$return = $rb_ext->account_list( args( "wallet" => $walletID ) ); // Get all accounts of a wallet
+		
+		// Fetch every account
+		
+		foreach($return["accounts"] as $account){
+		
+			$return2 = $rb_ext->account_balance( args( "account" => $account ) ); // Get balance of account
+			
+			$accounts_balances["accounts"][$account] = array( // Build the return array
+			
+				"balance_rai" => floor( $return2["balance"]/RAIN ),
+				"pending_rai" => floor( $return2["pending"]/RAIN )
+			
+			);
+			
+			$accounts_balances["sum_balance_rai"] += $accounts_balances["accounts"][$account]["balance_rai"];
+			$accounts_balances["sum_pending_rai"] += $accounts_balances["accounts"][$account]["pending_rai"];
+		
+		}
+		
+		return $accounts_balances;
+	
+	}
+	
+	// Call this function to clear a wallet sending all funds to an account
+	// Parameters:
+	// $walletID -> the ID of the wallet you want to clear
+	// $destination -> the account that receive all funds
+	
+	function raiblocks_clear_wallet( $walletID, $destination ){
+	
+		global $rb_ext;
+		$payment_hashes = array( "sum_balance_rai" => 0, "sum_paid_rai" => 0, "accounts" => array() );
+		
+		$return = raiblocks_balance_wallet($walletID);
+		
+		$payment_hashes["sum_balance_rai"] = $return["sum_balance_rai"];
+		
+		foreach( $return["accounts"] as $account => $balance ){
+			
+			if( $balance["balance_rai"] > 0 ){
+			
+				$args = array(
+				
+					"wallet" => $walletID,
+					"source" => $account,
+					"destination" => $destination,
+					"amount" => $balance["balance_rai"].RAI
+				
+				);
+				
+				$return2 = $rb_ext->send( $args );
+				
+				if( $return2["block"] != "0000000000000000000000000000000000000000000000000000000000000000" ){ // If payment performed correctly
+				
+					$payment_hashes["accounts"][$account] = array(
+					
+						"hash" => $return2["block"],
+						"amount" => $balance["balance_rai"]
+					
+					);
+					
+					$payment_hashes["sum_paid_rai"] += $balance["balance_rai"];
+				
+				}else{ // If error happened
+				
+					$payment_hashes["accounts"][$account] = array(
+					
+						"hash" => "error",
+						"amount" => $balance["balance_rai"]
+					
+					);
+				
+				}
+			
+			}
+			
+		}
+		
+		return $payment_hashes;
+	
+	}
+	
+	// Call this function to send funds from a wallet to an account without sending from a particular account
+	// Parameters:
+	// $walletID -> the ID of the wallet you want to use as soruce of payment
+	// $destination -> the account to send funds
+	// $amount -> the funds you want to send (rai)
+	
+	function raiblocks_send_wallet( $walletID, $destination, $amount ){
+	
+		global $rb_ext;
+		$payment_hashes = array( "status" => "ok", "sum_paid_rai" => 0, "accounts" => array() ); $selected_accounts = array(); $sum = 0; $diff_amount = $amount;
+		
+		$return = raiblocks_balance_wallet($walletID);
+		
+		// Select funds from accounts
+		
+		foreach($return["accounts"] as $account => $balance){
+		
+			if( $balance["balance_rai"] > 0 ){
+			
+				$selected_accounts[$account] = $balance["balance_rai"];
+				$sum += $balance["balance_rai"];
+			
+			}else{
+			
+				continue;
+			
+			}
+			
+			if($sum >= $amount) break; // Amount reached?
+		
+		}
+		
+		// Sum not reached?
+		
+		if( $sum < $amount ){
+		
+			$payment_hashes["sum_paid_rai"] = 0;
+			$payment_hashes["status"] = "not enough funds";
+			return $payment_hashes;
+		
+		}
+		
+		// Sum reached?
+		
+		foreach($selected_accounts as $selected_account => $balance){
+			
+			if( $diff_amount - $balance < 0 ){
+			
+				$balance = $diff_amount;
+			
+			}else{
+			
+				// Nothing.
+			
+			}
+			
+			$args = array(
+			
+				"wallet" => $walletID,
+				"source" => $selected_account,
+				"destination" => $destination,
+				"amount" => $balance.RAI
+			
+			);
+			
+			$return2 = $rb_ext->send( $args );
+			
+			if( $return2["block"] != "0000000000000000000000000000000000000000000000000000000000000000" ){ // If payment performed correctly
+			
+				$payment_hashes["accounts"][$selected_account] = array(
+				
+					"hash" => $return2["block"],
+					"amount" => $balance
+				
+				);
+				
+				$payment_hashes["sum_paid_rai"] += $balance;
+				
+				$diff_amount -= $balance;
+			
+			}else{ // If error happened
+			
+				$payment_hashes["accounts"][$selected_account] = array(
+				
+					"hash" => "error",
+					"amount" => $balance
+				
+				);
+				
+				$payment_hashes["status"] = "error";
+			
+			}
+		
+		}
+		
+		return $payment_hashes;
+		
+	}
+	
+?>
